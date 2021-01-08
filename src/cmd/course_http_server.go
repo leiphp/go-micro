3fd6a24d1e7cf57426008377f51d79eacd4ac552
leiwen <leiwen@jmfen.cn>
2021-01-08 17:18:08 +0800

    go-micro快速集成gin框架开发http api

--- src/cmd/course_http_server.go
+++ src/cmd/course_http_server.go
@@ -5,21 +5,31 @@ import (
 	"github.com/micro/go-micro/v2/client/grpc"
 	"github.com/micro/go-micro/v2/web"
 	"go-micro/src/Course"
+	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
 )
 
 
 func main(){
-	service := web.NewService(
-		web.Name("api.100txy.com.http.course"))
+	//go-micro集成gin开发http api
 	c := grpc.NewClient()
-	service.HandleFunc("/test",func(writer http.ResponseWriter, request *http.Request){
+	r := gin.New()
+	r.Handle("GET","/test", func(ctx *gin.Context){
 		c := Course.NewCourseService("api.100txy.com.course",c)
 		course_rsp,_ := c.ListForTop(context.Background(),&Course.ListRequest{Size:10})
-		log.Println(course_rsp.Result)
-		writer.Write([]byte("http api test"))
+		ctx.JSON(200, gin.H{"Result":course_rsp.Result})
 	})
+
+	service := web.NewService(
+		web.Name("api.100txy.com.http.course"),
+		web.Handler(r),
+		)
+	//service.HandleFunc("/test",func(writer http.ResponseWriter, request *http.Request){
+	//	c := Course.NewCourseService("api.100txy.com.course",c)
+	//	course_rsp,_ := c.ListForTop(context.Background(),&Course.ListRequest{Size:10})
+	//	log.Println(course_rsp.Result)
+	//	writer.Write([]byte("http api test"))
+	//})
 	service.Init()
 
 	if err := service.Run(); err != nil {
