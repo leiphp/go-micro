4e9957f9dd96a6cb8ff6bb5c015a06095d5dd673
leiwen <leiwen@jmfen.cn>
2021-01-14 13:05:49 +0800

    feat：连接数据库拿数据

--- src/service/CourseService.go
+++ src/service/CourseService.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"context"
+	"go-micro/src/Boot"
 	."go-micro/src/Course"
+	"go-micro/src/Vars"
 )
 
 func NewCourseModel (id int32, name string) *CourseModel {
@@ -14,9 +16,17 @@ type CourseServiceImpl struct {
 }
 
 func (this *CourseServiceImpl) ListForTop(ctx context.Context, req *ListRequest, rsp *ListResponse)  error {
-	ret := make([]*CourseModel,0)
-	ret = append(ret,NewCourseModel(101,"java课程"),NewCourseModel(102,"php课程"))
-	rsp.Result = ret
+	//ret := make([]*CourseModel,0)
+	//ret = append(ret,NewCourseModel(101,"java课程"),NewCourseModel(102,"php课程"))
+	//rsp.Result = ret
+	//return nil
+
+	course := make([]*CourseModel,0)
+	err := Boot.GetDB().Table(Vars.Table_CourseMain).Order("course_id desc").Find(&course).Error
+	if err != nil {
+		return err
+	}
+	rsp.Result = course
 	return nil
 }
 
