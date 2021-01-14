540025c6e6e8aa8320ad8359a003ee349f27262f
leiwen <leiwen@jmfen.cn>
2021-01-14 16:57:52 +0800

    feat：完善路由模块

--- src/cmd/gateway.go
+++ src/cmd/gateway.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"go-micro/src/gateway"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
 
 func main() {
+	routes := gateway.InitConfig()
+	//fmt.Println(routes[0],routes[1])
 	http.HandleFunc("/",func(writer http.ResponseWriter, request *http.Request) {
-		remote, _ := url.Parse("http://"+"localhost:9000")
-		p := httputil.NewSingleHostReverseProxy(remote)
-		p.ServeHTTP(writer, request)
+		if r := routes.Match(request);r!=nil {
+			remote, _ := url.Parse(r.Url)
+			//request.URL.Path /v1/detail/1 => /detail/1去掉v1
+			request.URL.Path = strings.Replace(request.URL.Path,"/v1","",-1)
+			p := httputil.NewSingleHostReverseProxy(remote)
+			p.ServeHTTP(writer, request)
+		}else{
+			writer.WriteHeader(http.StatusNotFound)
+		}
+
 	})
 	http.ListenAndServe(":8123",nil)
 }
---
+++ src/gateway/init.go
@@ -0,0 +1,20 @@
+package gateway
+
+import (
+	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-micro/v2/util/log"
+)
+
+func InitConfig() Routes {
+	configFile := "gateway.yaml"
+	err := config.LoadFile(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := make(Routes,0)
+	err=config.Get("routes").Scan(&r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return r
+}
