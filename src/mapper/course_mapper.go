35aa96c9ea8099eaf6e60b5c7938fbde17f5c286
leiwen <leiwen@heytea.com>
2021-07-08 18:16:20 +0800

    feat:新增课程详情

--- src/mapper/course_mapper.go
+++ src/mapper/course_mapper.go
@@ -6,11 +6,18 @@ import (
 	"go-micro/src/vars"
 )
 
+//gorm获取列表
 func GetCourseList() *gorm.DB{
 	return Boot.GetDB().Table(vars.Table_CourseMain).Order("course_id desc").Limit(3)
 }
 
+//原生sql获取列表
 const course_list="select * from course_main order by course_id desc limit ?"
 func GetCourseListBySql(args ...interface{}) *gorm.DB{
 	return Boot.GetDB().Raw(course_list,args...)
+}
+
+//获取课程详情
+func GetCourseDetail(course_id int) *gorm.DB{
+	return Boot.GetDB().Table(vars.Table_CourseMain).Where("course_id = ?",course_id)
 }
